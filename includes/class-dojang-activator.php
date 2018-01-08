dc1cbb8f38d68b517a2b1ed845de989271af6a02
PJG <pjgumulka@gmail.com>
2018-01-08 19:11:23 +0100

    Update class-dojang-activator.php

--- includes/class-dojang-activator.php
+++ includes/class-dojang-activator.php
@@ -32,6 +32,7 @@ class Dojang_Activator {
 	public static function activate() {
 		global $wpdb;
 		$prefix= $wpdb->prefix;
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		$table_name= $prefix.'groupplayers';
 		$sql = "
 		CREATE TABLE IF NOT EXISTS $table_name (
@@ -45,7 +46,72 @@ class Dojang_Activator {
 		  PRIMARY KEY  (id)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=267;
 		";
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta($sql);
+		
+		$table_name= $prefix.'groups';
+		$sql = "
+		CREATE TABLE IF NOT EXISTS $table_name (
+  		  id int(11) NOT NULL AUTO_INCREMENT,
+  		  groupName text NOT NULL,
+  		  groupLeagueId int(11) NOT NULL,
+  		  playerGroupId int(11) NOT NULL,
+  		  groupOrder int(11) NOT NULL,
+  		  isGroupLocked int(11) NOT NULL,
+  		  PRIMARY KEY  (id)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=42;
+		";
+		dbDelta($sql);
+		
+		$table_name= $prefix.'leagues';
+		$sql = "
+		CREATE TABLE IF NOT EXISTS $table_name (
+  		  id int(11) NOT NULL AUTO_INCREMENT,
+  		  leagueName text NOT NULL,
+  		  hidden tinyint(1) NOT NULL,
+  		  closed tinyint(1) NOT NULL,
+  		  pointsDistributed int(11) NOT NULL COMMENT 'if > 0 then points were distributed to players and league is completed',
+    		  PRIMARY KEY  (id)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=12; 
+		";
+		dbDelta($sql);
+		
+		$table_name= $prefix.'players';
+		$sql = "
+			CREATE TABLE IF NOT EXISTS $table_name (
+			  playerId int(11) NOT NULL AUTO_INCREMENT,
+			  studentId int(11) NOT NULL,
+			  playerName varchar(100) NOT NULL,
+			  playerEmail varchar(50) NOT NULL,
+			  playerKgs varchar(20) NOT NULL,
+			  playerRank varchar(10) NOT NULL,
+			  playerCountry varchar(50) NOT NULL,
+			  playerTimezone text NOT NULL COMMENT 'timezone i.e. +2 or -6 in relation to GMT',
+			  playerApproved int(11) NOT NULL COMMENT 'approval means that player will be visible in creation of new league group',
+			  PRIMARY KEY  (playerId)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=398;		
+		";
+		dbDelta($sql);
+	
+	
+		$table_name= $prefix.'results';
+		$sql = "
+			CREATE TABLE IF NOT EXISTS $table_name (
+			  REATE TABLE nresults (
+			  id int(11) NOT NULL,
+			  groupId int(11) NOT NULL,
+			  playerIdBlack int(11) NOT NULL,
+			  playerIdWhite int(11) NOT NULL,
+			  playerIdWinner int(11) NOT NULL,
+			  file text NOT NULL,
+			  linkKGS text NOT NULL,
+			  commentedGameId int(11) NOT NULL,
+			  addDate date NOT NULL,
+			  isApproved tinyint(4) NOT NULL,
+			  result text NOT NULL,
+			  link mediumtext NOT NULL,
+			  oldResultId int(11) NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=652;
+		";
 		dbDelta($sql);
 	}
 
