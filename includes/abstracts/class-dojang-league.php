3ec347a304c813080b1344c7e3b9a02ed50cad73
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-12 23:37:19 +0100

    change in documentation

--- includes/abstracts/class-dojang-league.php
+++ includes/abstracts/class-dojang-league.php
@@ -1,28 +1,20 @@
 <?php
 
 /**
- * The file that defines the core plugin class
+ * Class responsible for High Level League management
  *
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
+ * Class responsible for High Level League management
  *
  * @link       https://www.linkedin.com/in/piotr-jacek-gumulka/
- * @since      1.0.0
+ * @since      2.1.1
  *
  * @package    Dojang
- * @subpackage Dojang/includes
+ * @subpackage Dojang/includes/abstracts
  */
 
 /**
- * The core plugin class.
  *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
- * @since      1.0.0
+ * @since      2.1.1
  * @package    Dojang
  * @subpackage Dojang/includes
  * @author     Piotr Jacek Gumulka <pjgumulka@gmail.com>
