244410d70ff68fd5a4fb8862a351b4b6b6c0ee62
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-13 23:12:29 +0100

    adding player class

--- includes/abstracts/class-dojang-group.php
+++ includes/abstracts/class-dojang-group.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Class responsible for High Level League management
+ * Class responsible for High Level Group management
  *
  * It contains methods to get group details and as such works with groups and groupplayers tables in database
  *
@@ -19,12 +19,16 @@
  * @subpackage Dojang/includes/abstracts
  * @author     Piotr Jacek Gumulka <pjgumulka@gmail.com>
  */
+require_once('class-dojang-player.php');
 class Dojang_Group {
   public $groupDetails;
   public $groupPlayers;
   public function __construct($groupId){
-  global $wpdb;
-	$this->groupDetails= $wpdb->get_results("SELECT * FROM {$wpdb->prefix}groups WHERE id = $groupId")[0];
-  $this->groupPlayers= $wpdb->get_results("SELECT * FROM {$wpdb->prefix}groupplayers WHERE playerGroupId = {$this->groupDetails->playerGroupId}");
+    global $wpdb;
+	   $this->groupDetails= $wpdb->get_results("SELECT * FROM {$wpdb->prefix}groups WHERE id = $groupId")[0];
+     $this->groupPlayers= $wpdb->get_results("SELECT * FROM {$wpdb->prefix}groupplayers WHERE playerGroupId = {$this->groupDetails->playerGroupId}");
+     foreach($this->groupPlayers as $p){
+       $p[] = Dojang_Player($p->id);
+     }
   }
 }
---
+++ includes/abstracts/class-dojang-player.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class responsible for High Level Players management
+ *
+ * It contains methods to get players details and as such works with players table in database
+ *
+ * @link       https://www.linkedin.com/in/piotr-jacek-gumulka/
+ * @since      2.1.1
+ *
+ * @package    Dojang
+ * @subpackage Dojang/includes/abstracts
+ */
+
+/**
+ *
+ * @since      2.1.1
+ * @package    Dojang
+ * @subpackage Dojang/includes/abstracts
+ * @author     Piotr Jacek Gumulka <pjgumulka@gmail.com>
+ */
+class Dojang_Player {
+  public $playerDetails;
+  public function __construct($playerId){
+    global $wpdb;
+    $this->playerDetails= $wpdb->get_results("SELECT * FROM {$wpdb->prefix}players WHERE id = $playerId")[0];
+  }
