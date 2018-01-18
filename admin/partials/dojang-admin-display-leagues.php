545baa5b6eefe65a37881d539217e91c9657247b
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-18 23:50:18 +0100

    removed stupid clutter

--- admin/partials/dojang-admin-display-leagues.php
+++ admin/partials/dojang-admin-display-leagues.php
@@ -16,10 +16,5 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
 <div class="wrap">
-	    <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-	<pre><? print_r( $this->test_function_from_admin() ); ?></pre>
-	<pre><? print_r( $this->test_2() ); ?></pre>
-	
-	<pre><? print_r( $this->get_current_league_details() ); ?></pre>
 	    
 </div>
