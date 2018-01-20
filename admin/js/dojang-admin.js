25ba86b3da56e40ed7c653589e57d66c2073e603
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-20 21:40:27 +0100

    removed long unnecessary comment in JS

--- admin/js/dojang-admin.js
+++ admin/js/dojang-admin.js
@@ -1,34 +1,7 @@
 (function( $ ) {
 	'use strict';
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-	/*Baduk Dojank Main View, change not approved results in table when hover over Games to Approve list*/
 	$(function() {
+		/*Baduk Dojank Main View, change not approved results in table when hover over Games to Approve list*/
 		$('.dojang-games-to-approve tbody tr')
 			.mouseenter(function(){
 				var result_id= $(this).attr('x-result-id');
