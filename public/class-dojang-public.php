e78228b0e163670095041397916e919c97d54381
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-24 00:11:17 +0100

    order archive by latest closed league

--- public/class-dojang-public.php
+++ public/class-dojang-public.php
@@ -136,7 +136,7 @@ class Dojang_Public {
 	}
 	public function renderArchive(){
 		global $wpdb;
-		$archiveLeaguesIds= $wpdb->get_col("SELECT id AS pastLeagues FROM {$wpdb->prefix}leagues WHERE closed = 1");
+		$archiveLeaguesIds= $wpdb->get_col("SELECT id AS pastLeagues FROM {$wpdb->prefix}leagues WHERE closed = 1 ORDER BY pastLeagues");
 		$html='';
 		foreach($archiveLeaguesIds as $leagueId){
 			$league= new Dojang_League($leagueId);
