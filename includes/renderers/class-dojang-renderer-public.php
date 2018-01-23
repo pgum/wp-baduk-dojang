13a5dbd8d9e5d2018b80504f0fd245a956264240
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-23 23:38:43 +0100

    copy paste to hangle league object as argument for render league

--- includes/renderers/class-dojang-renderer-public.php
+++ includes/renderers/class-dojang-renderer-public.php
@@ -13,6 +13,17 @@ class Dojang_Renderer_Public{
     $registerRenderer= new Dojang_Renderer_Registration();
     return $registerRenderer->renderRegisterForm();
   }
+  public function renderLeague($league){
+    $groupArray= $league->getGroupsDetails();
+    $html='<h2 class="dojang-results"> Group Standings</h2>';
+    foreach ($groupArray as $group){
+      $gR = new Dojang_Renderer_Group_Public($group);
+      $html.= $gR->renderGroupInfo();
+      $html.= $gR->renderGroupTable();
+      $html.='<br/>';
+    }
+    return $html;
+  }
   public function renderCurrentLeague(){
     $league= new Dojang_League();
     $groupArray= $league->getGroupsDetails();
