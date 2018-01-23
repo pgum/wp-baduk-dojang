592f869b39623c0f65e10b950cd12963b0415191
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-23 19:43:16 +0100

    class names and additional container for notice function

--- includes/renderers/class-dojang-renderer-public.php
+++ includes/renderers/class-dojang-renderer-public.php
@@ -15,19 +15,19 @@ class Dojang_Renderer_Public{
 
   }
   public function renderPlayerNotRegisteredNotice($msg){
-    $html='<div class="dojang-notice">';
+    $html='<div class="dojang-notice"><div>';
     $html.='<h2 class="dojang-nok">Your registration had errors!</h2>';
     $html.='<p>Please try again, and remember that all fields are required!</p>';
     $html.='<p>'.$msg.'</p>';
-    $html.='</div>';
+    $html.='</div></div>';
     return $html;
   }
 
   public function renderPlayerRegisteredNotice(){
-    $html='<div class="dojang-notice">';
+    $html='<div class="dojang-notice"><div>';
     $html.='<h2 class="dojang-ok">Your registration is saved and waiting for approval!</h2>';
     $html.='<p>We will write to e-mail address you provided. When your registration is approved account details will be send there too!</p>';
-    $html.='</div>';
+    $html.='</div></div>';
     return $html;
   }
   public function renderRegisterForm(){
@@ -38,22 +38,23 @@ class Dojang_Renderer_Public{
     $prev_rank=   isset($_GET['prev-rank'])   ? $_GET['prev-rank']    : "";
 
     $html = '<h2 class="dojang-register">Registration Form</h2>';
+    $html.= '<p>Please fill out form below. All fields marked with <span class="dojang-required">*</span> are required for your form to be submitted.</p>'
     $html.='<form class="dojang-register-form" action="'.get_admin_url().'admin-post.php" method="post">
       <input type="hidden" name="action" value="dojang_register" />
       <fieldset>
-        <div class="dojang-form-label"><label for="dojang-player-name">Player Name<span class="required">*</span></label></div>
+        <div class="dojang-form-label"><label for="dojang-player-name">Player Name<span class="dojang-required">*</span></label></div>
         <div class="dojang-form-input"><input name="dojang-player-name" type="text" value="'.$prev_name.'" placeholder="Name that will be visible in Players List"></div>
 
-        <div class="dojang-form-label"><label for="dojang-player-email">Valid Email ()<span class="required">*</span></label></div>
+        <div class="dojang-form-label"><label for="dojang-player-email">Valid Email<span class="dojang-required">*</span></label></div>
         <div class="dojang-form-input"><input name="dojang-player-email" type="text" value="'.$prev_email.'" placeholder="E-mail that will be used to contact"></div>
 
-        <div class="dojang-form-label"><label for="dojang-player-kgs-account">KGS Account <span class="required">*</span></label></div>
+        <div class="dojang-form-label"><label for="dojang-player-kgs-account">KGS Account <span class="dojang-required">*</span></label></div>
         <div class="dojang-form-input"><input name="dojang-KGS-account" type="text" value="'.$prev_kgs.'" placeholder="KGS that will be used in league games"></div>
 
-        <div class="dojang-form-label"><label for="dojang-player-rank">Rank <span class="required">*</span></label></div>
+        <div class="dojang-form-label"><label for="dojang-player-rank">Rank <span class="dojang-required">*</span></label></div>
         <div class="dojang-form-input"><select name="dojang-player-rank">'.$this->renderRankOptions($prev_rank).'</select></div>
 
-        <div class="dojang-form-label"><label for="dojang-player-country">Country <span class="required">*</span></label></div>
+        <div class="dojang-form-label"><label for="dojang-player-country">Country <span class="dojang-required">*</span></label></div>
         <div class="dojang-form-input"><input name="dojang-player-country" type="text" value="'.$prev_country.'" placeholder="What country are you from"></div>
       </fieldset>
       <fieldset>
