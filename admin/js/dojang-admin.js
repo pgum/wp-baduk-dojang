5aea07f54376a11e64a707236d56e6ff93851fa7
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-28 22:13:38 +0100

    fix highlight

--- admin/js/dojang-admin.js
+++ admin/js/dojang-admin.js
@@ -17,7 +17,7 @@
 	});
 	$(function() {
 		/*Baduk Dojank Main View, change not approved results in table when hover over Games to Approve list*/
-		$('.dojang-games-to-approve tbody tr')
+		$('.dojang-table-to-approve tbody tr')
 			.mouseenter(function(){
 				var result_id= $(this).attr('x-result-id');
 	     	$('td[x-result-id='+result_id+']').addClass('dojang-result-highlight');
