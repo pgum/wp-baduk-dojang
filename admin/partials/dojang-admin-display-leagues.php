f66dac316f4a7727ad92b710b22938259751774b
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-28 18:14:44 +0100

    fix

--- admin/partials/dojang-admin-display-leagues.php
+++ admin/partials/dojang-admin-display-leagues.php
@@ -24,11 +24,10 @@ $html='';
 foreach($archiveLeaguesIds as $leagueId){
 	$league= new Dojang_League($leagueId);
 	$renderer= new Dojang_Renderer($league);
-	$html.=$renderer->renderLeagueInfo().'<br/>';
 	$leagueClosed= $league->getLeagueInfo()->closed;
 	$archiveClass= ($leagueClosed==1) ? '-archive dojang-hidden' : '';
+	$html.=$renderer->renderLeagueInfo();
 	$html.='<div class="dojang-league-groups-results-tables'.$archiveClass.'" x-league-id="'.$leagueId.'">';
-	$html.=$renderer->renderLeagueCloseAndDistributePoints().'<br/>';
 	$html.=$renderer->renderGroupsTable().'<br/>';
 	$html.='</div>';
 }
