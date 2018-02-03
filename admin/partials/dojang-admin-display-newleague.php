8c1bc690a25c29aaee8fb204e5f7a59c19faefca
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-02-03 23:15:36 +0100

    proof-of-concept interface with mocks

--- admin/partials/dojang-admin-display-newleague.php
+++ admin/partials/dojang-admin-display-newleague.php
@@ -19,45 +19,98 @@
 	    <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 			<div id="dojang-editor">
 				<div id="dojang-toolbar"></div>
-				<div id="dojang-workspace"></div>
+				<div id="dojang-workspace">
+					<ul id="dojang-sortable">
+						<li>
+							<div class="dojang_group"></div>
+						</li>
+						<li>
+							<div class="dojang_group"></div>
+						</li>
+						<li>
+							<div class="dojang_group"></div>
+						</li>
+						<li>
+							<div class="dojang_group"></div>
+						</li>
+						<li>
+							<div class="dojang_group"></div>
+						</li>
+					</ul>
+				</div>
 			</div>
 </div>
+<?php
+//function areSavedDrafts(){
+//	return 0;
+//}
+?>
 <script>
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var d = new Date();
+//var hideSavedDrafts= <?php /*areSavedDrafts();*/ ?> != 0 ? true : false;
 $('#dojang-toolbar').w2toolbar({
 		name: 'toolbar',
 		items: [
-			{ type: 'button', id: 'new_group', text: 'New Empty Group', icon: 'dashicons dashicons-plus' },
-			{ type: 'break' },
-			{ type: 'html' html: 'Import from Current League:'},
-			{ type: 'button', id: 'from_group_91', text: 'Group A', icon: 'dashicons dashicons-form' },
-			{ type: 'button', id: 'from_group_92', text: 'Group B', icon: 'dashicons dashicons-form' },
-			{ type: 'button', id: 'from_group_93', text: 'Group C', icon: 'dashicons dashicons-form' },
-			{ type: 'button', id: 'from_group_94', text: 'Group D', icon: 'dashicons dashicons-form' },
-			{ type: 'break' },
-			{ type: 'menu-check', id: 'item3', text: 'Menu Check', icon: 'dashicons dashicons-heart',
-				selected: ['id3', 'id4'],
-				onRefresh: function (event) {
-					event.item.count = event.item.selected.length;
-				},
+			{ type: 'button', id: 'new_group', text: 'New Group', icon: 'dashicons dashicons-plus' },
+			{ type: 'menu', id: 'new_from_current', caption: 'Current Groups', icon: 'dashicons dashicons-forms',
 				items: [
-					{ id: 'id1', text: 'Item 1', icon: 'dashicons dashicons-camera' },
-					{ id: 'id2', text: 'Item 2', icon: 'dashicons dashicons-menu' },
-					{ id: 'id3', text: 'Item 3', icon: 'dashicons dashicons-menu', count: 12 },
-					{ text: '--' },
-					{ id: 'id4', text: 'Item 4', icon: 'dashicons dashicons-admin-tools' }
-				]
+<?php
+$currentLeague = new Dojang_League();
+$groups = $currentLeague->getGroupsDetails();
+foreach($groups as $g)
+	echo '{ id:\''.$g->groupDetails->playerGroupId.'\', text: \''.$g->groupDetails->groupName.'\', img: \'dashicons dashicons-arrow-right\' },';
+ ?>
+	      ]
+		  },
+			{ type: 'break' },
+			{ type: 'html',  id: 'league_multiplier', value: 1,
+				html: function (item) {
+					return '<div><span class="dashicons dashicons-no-alt"></span>Multiplier:'+
+								 '<input class="dojang-toolbar-input" onchange="var el = w2ui.toolbar.set(\'league_multiplier\', { value: this.value });" '+
+								 'size="3" placeholder="1,2,.." value="'+item.value+'"/></div>';
+				}
 			},
 			{ type: 'break' },
+			{ type: 'html',  id: 'league_name', value: monthNames[d.getMonth()],
+				html: function (item) {
+					return '<div><span class="dashicons dashicons-format-quote"></span>Name:'+
+								 '<input class="dojang-toolbar-input" onchange="var el = w2ui.toolbar.set(\'league_name\', { value: this.value });" '+
+								 'value="'+item.value+'" size="20" placeholder="eg. January..."/></div>';
+				}
+			},
 			{ type: 'spacer' },
-			//{ type: 'check', id: 'close_upon_save', text: 'Close Current League upon Saving', icon: 'dashicons dashicons-star-filled' },
-			{ type: 'check', id: 'save_draft', text: 'Save as Draft', icon: 'dashicons dashicons-admin-post' },
+			{ type: 'html', html: 'Save:'},
+			{ type: 'radio', id: 'save_as_draft', group: '1', text: 'as Draft', icon: 'dashicons dashicons-edit', checked: true },
+			{ type: 'radio', id: 'save_publish', group: '1', text: 'Publish on save', icon: 'dashicons dashicons-admin-post' },
 			{ type: 'break' },
-			{ type: 'button',  id: 'publish_league',  text: 'Save League', icon: 'dashicons dashicons-plus-alt' }
+			{ type: 'button',  id: 'save_button',  text: '<b>Confirm</b>', icon: 'dashicons dashicons-plus-alt' }
 	]
 });
 
-w2ui.toolbar.on('*', function (event) {
-		console.log('EVENT: '+ event.type + ' TARGET: '+ event.target, event);
+function createNewGroup(gid=null){
+	if(gid){console.log(gid);}
+	else{console.log('new group');}
+}
+w2ui.toolbar.on('click', function (event) {
+		//console.log('EVENT: '+ event.type + ' TARGET: '+ event.target, event);
+		if(event.target == 'new_group'){
+			console.log('Create New Group');
+			createNewGroup();
+		}
+		if(event.target.match(/new_from_current:[0-9]*/)){
+			var group_id=event.target.split(':')[1];
+			console.log('Group Id to Clone= '+group_id);
+			createNewGroup(group_id);
+		}
+		if(event.target == 'save_button'){
+			var league_multiplier= w2ui.toolbar.get('league_multiplier').value;
+			var league_name= w2ui.toolbar.get('league_name').value;
+			var draft_or_publish= w2ui.toolbar.get('save_as_draft').checked ? 'draft' : 'publish';
+			console.log('save clicked: name='+league_name+' multiplier='+league_multiplier+' will be: '+draft_or_publish);
+		}
 });
-
 </script>
